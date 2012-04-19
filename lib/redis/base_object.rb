0bc69d94aea65d4ffbd1bc9e965a959d9f101e0a
Obie Fernandez <obie@hashrocket.com>
2012-04-19 19:17:50 -0300

    Give to_ary method and default to marshaling

--- lib/redis/base_object.rb
+++ lib/redis/base_object.rb
@@ -3,10 +3,14 @@ class Redis
   class BaseObject
     def initialize(key, *args)
       @key     = key.is_a?(Array) ? key.flatten.join(':') : key
-      @options = args.last.is_a?(Hash) ? args.pop : {}
+      @options = args.last.is_a?(Hash) ? args.pop : {marshal: true}
       @redis   = args.first || $redis || Redis.current
     end
 
     alias :inspect :to_s  # Ruby 1.9.2
+    
+    def to_ary
+      respond_to?(:to_a) ? to_a : [self]
+    end
   end
 end
